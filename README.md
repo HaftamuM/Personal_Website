68322894f7ed28a886eca959a54ebdd7bcca4c93
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2023-05-04 13:19:19 -0400

    A

--- README.md
+++
@@ -1,7 +0,0 @@
-# Personal_Website
-Personal web page 
-
-A personal website that show some of my experience and achievement through the four year of being a software developer. 
-
-Website hosted in github 
-
--- script.js
+++ script.js
@@ -1,17 +1,48 @@
-$("document").ready(function () {
-  $("img").addClass("animated fadeInDown");
-  $("h1").addClass("animated fadeInDown");
-  $("h2").addClass("animated fadeInDown");
-  $("h3").addClass("animated fadeInDown");
-  $("h4").addClass("animated fadeInDown");
-  $("h5").addClass("animated fadeInDown");
-  $("h6").addClass("animated fadeInDown");
-  $("p").addClass("animated fadeInDown");
-  $("hr").addClass("animated fadeInDown");
-  $("i").addClass("animated fadeInDown");
-  $("section").addClass("animated fadeInDown");
-  $("article").addClass("animated fadeInDown");
-  $("ul").addClass("animated fadeInDown");
-  
-  $.scrollIt();
+$(function() {
+	setupNavbar();
+	setupContactFormSubmission();
+	renderThumbnails();	
 });
+
+function setupNavbar() {
+	$("#nav li").click(function() {
+		var target = $(this).data("target");
+		$("html, body").animate({
+			scrollTop: $(target).offset().top }, 500);
+	});
+}
+
+function setupContactFormSubmission() {
+	$("#contactForm input[type=submit]").click(function(e) {
+		e.preventDefault();
+		alert("Not ready yet!");
+	});
+}
+
+
+function createThumbnail(thumbnail) {
+	var html = $("#templates .thumbnail-wrapper").clone();
+	html.find(".thumbnail-title span").text(thumbnail.title);
+	html.find(".thumbnail-image img").attr("src", thumbnail.artworkURL);
+	html.find(".thumbnail-caption a").attr("href", thumbnail.authorURL).text(thumbnail.author);
+
+	return html;
+}
+// Add new js file - 
+$(document).ready(function() {
+	// executes when HTML-Document is loaded and DOM is ready
+   console.log("document is ready");
+	 
+   
+	 $( ".card" ).hover(
+	 function() {
+	   $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
+	 }, function() {
+	   $(this).removeClass('shadow-lg');
+	 }
+   );
+	 
+   // document ready  
+   });
+   // Baro 
+   
