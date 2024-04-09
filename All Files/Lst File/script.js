6f3502236bb6c4c66bcd304130aa6e404d8e0096
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-04-09 21:32:16 +0000

    Change of rm All Files:

--- All Files/Lst File/script.js
+++
@@ -1,1 +0,0 @@
-// Uses rem for size so go ahead and change the font-size of html and see the buttons get bigger (or smaller...) :)
--- All Files/Website/script.js
+++
@@ -1,9 +0,0 @@
-$(window).resize(function() { 
-  
-  if ($(window).width() > 928) {
-    $('aside').height($('section').height())
-  } else {
-    $('aside').height('auto')
-  }
-   
-});
